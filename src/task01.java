a00ac124926354cc3028a5793b9d815c5dda2e54
Dima <spacecape@gmail.com>
2021-10-10 23:58:24 +0300

    update

--- src/task01.java
+++ src/task01.java
@@ -1,5 +1,8 @@
 public class task01 {
     public static void main(String[] args) {
-        System.out.println("Hello QA");
+        int account = 2_000_000_000;
+        int transfer = 500_000_000;
+        int total = account + transfer;
+        System.out.println(total);
     }
 }
